6e38c4b9a9e1e00cf1cd87abe2201616bb5d5a60
Filipe Pina <fopina@skmobi.com>
2015-03-17 22:04:03 +0000

    initial index view setup

--- cowepl/urls.py
+++ cowepl/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+import views
+
 urlpatterns = patterns('',
-    url(r'^$', include('cowepl.urls', namespace='cowepl')),
+    url(r'^$', views.index),
 )
